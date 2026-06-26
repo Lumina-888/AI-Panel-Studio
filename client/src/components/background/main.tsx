9c65a4de4c42ab711ef87870cbb2060f96447d09
Lumina <2308770119@mails.qust.edu.cn>
2026-06-26 18:44:54 +0800

    feat: add theme prop to StarfieldBackground for mono/blue gradient support

--- client/src/components/background/main.tsx
+++ client/src/components/background/main.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './main.css'
 
-const StarfieldBackground: React.FC = () => {
+interface StarfieldBackgroundProps {
+  theme?: 'mono' | 'blue'
+}
+
+const StarfieldBackground: React.FC<StarfieldBackgroundProps> = ({ theme = 'blue' }) => {
   return (
-    <div className="starfield-container">
+    <div className={`starfield-container ${theme === 'mono' ? 'sf-mono' : 'sf-blue'}`}>
       <div id="stars" />
       <div id="stars2" />
       <div id="stars3" />
